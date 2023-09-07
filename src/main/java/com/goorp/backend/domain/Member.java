647696db7e38e1972a81e801c67f7829115ad8ec
hyunseong hwang <ree7243@gmail.com>
2023-09-07 12:39:18 +0900

    #6 - member entity, repository, controller
    member 엔티티 설정
    리파지토리는 JpaRepository<Member, Long> 확장해서사용했지만 이후 직접 개발예정

---
+++ src/main/java/com/goorp/backend/domain/Member.java
@@ -0,0 +1,32 @@
+package com.goorp.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, length = 20)
+    private String memberId;
+    @Column(nullable = false, length = 60)
+    private String password;
+    @Column(nullable = false, length = 20)
+    private String name;
+//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Integer> posts = new ArrayList<>();
+    @Column(nullable = false)
+    private LocalDate createdAt;
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+}
