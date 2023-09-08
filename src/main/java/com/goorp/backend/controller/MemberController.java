02900315ff367270c5e9b86d302f148890fb04a6
hyunseong hwang <ree7243@gmail.com>
2023-09-09 00:31:23 +0900

    #11 - 멤버 dto validation 애노테이션 추가와 오류 메시지 설정
    멤버 응답을 검증하기위해 멤버 dto에 validation annotation 추가와 오류 메시지 설정

--- src/main/java/com/goorp/backend/controller/MemberController.java
+++ src/main/java/com/goorp/backend/controller/MemberController.java
@@ -1,15 +1,19 @@
 package com.goorp.backend.controller;
 
+import com.goorp.backend.dto.ApiResponseDto;
 import com.goorp.backend.dto.MemberJoinDto;
 import com.goorp.backend.dto.MemberLoginDto;
 import com.goorp.backend.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -19,16 +23,21 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody MemberJoinDto dto) {
+    public ApiResponseDto join(@Validated @RequestBody MemberJoinDto dto) {
         memberService.join(dto.getMemberId(), dto.getPassword(), dto.getPasswordConfirm(), dto.getMemberName());
-//        log.info("회원가입 성공");
-        return ResponseEntity.ok("회원가입 성공");
+        return ApiResponseDto.builder()
+                .ok(true)
+                .data(Map.of("message", "회원가입 성공"))
+                .build();
     }
-
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody MemberLoginDto dto) {
-        log.info("로그인 컨트롤러 실행");
+    public ApiResponseDto login(@Validated @RequestBody MemberLoginDto dto) {
         String token = memberService.login(dto.getMemberId(), dto.getPassword());
-        return ResponseEntity.ok(token);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("token", token);
+        return ApiResponseDto.builder()
+                .ok(true)
+                .data(Map.of("token", token))
+                .build();
     }
 }
