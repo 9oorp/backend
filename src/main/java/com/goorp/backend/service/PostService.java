db862936eb96986343fdba21c02afaa1cb31276a
wlstnam <wlstnam99@gmail.com>
2023-09-13 17:56:21 +0900

    #23 예외처리 변경

--- src/main/java/com/goorp/backend/service/PostService.java
+++ src/main/java/com/goorp/backend/service/PostService.java
@@ -1,19 +1,19 @@
 package com.goorp.backend.service;
 
 import com.goorp.backend.domain.Curriculum;
+import com.goorp.backend.domain.Member;
 import com.goorp.backend.domain.Post;
 import com.goorp.backend.dto.PostRequestDTO;
 import com.goorp.backend.dto.PostResponseDTO;
 import com.goorp.backend.exception.ErrorCode;
 import com.goorp.backend.exception.PostException;
 import com.goorp.backend.repository.CurriculumRepository;
+import com.goorp.backend.repository.MemberRepository;
 import com.goorp.backend.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,49 +23,56 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final CurriculumRepository curriculumRepository;
+    private final MemberRepository memberRepository;
     @Autowired
-    public PostService(PostRepository postRepository, CurriculumRepository curriculumRepository) {
+    public PostService(PostRepository postRepository, CurriculumRepository curriculumRepository, MemberRepository memberRepository) {
         this.postRepository = postRepository;
         this.curriculumRepository = curriculumRepository;
+        this.memberRepository = memberRepository;
     }
 
     // CREATE
     @Transactional
-    public PostResponseDTO createPost(Long curriculumId, PostRequestDTO requestDTO) {
+    public void createPost(PostRequestDTO requestDTO, Long curriculumId, Long memberId) {
         Curriculum curriculum = curriculumRepository.findById(curriculumId)
-//                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 커리큘럼 ID 입니다"));
-                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, "올바르지 않은 커리큘럼 ID 입니다"));
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, curriculumId + " 가 없습니다."));
 
-        Post post = convertToEntity(requestDTO).toBuilder().curriculum(curriculum).build();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, memberId + " 가 없습니다."));
+
+        Post post = convertToEntity(requestDTO).toBuilder()
+                .curriculum(curriculum)
+                .member(member)
+                .build();
+
         Post savedPost = postRepository.save(post);
-        return convertToResponseDTO(savedPost);
+        convertToResponseDTO(savedPost);
     }
 
     // READ
-    public PostResponseDTO findPostByCurriculumAndId(Long curriculumId, Long postId) {
-        // Ensure Curriculum exists
-        curriculumRepository.findById(curriculumId)
-                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 커리큘럼 ID 입니다"));
-
-        Post post = postRepository.findByCurriculumIdAndId(curriculumId, postId)
-                .orElseThrow(() -> new IllegalArgumentException("검색 결과가 존재하지 않습니다"));
-        return convertToResponseDTO(post);
+    public void findPostById(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, postId + " 가 없습니다."));
+        convertToResponseDTO(post);
     }
 
-    // READ
-    public List<PostResponseDTO> findAllPostsByCurriculum(Long curriculumId) {
-        List<Post> posts = postRepository.findByCurriculumId(curriculumId);
-        return posts.stream().map(this::convertToResponseDTO).collect(Collectors.toList());
-    }
+//    // READ
+//    public List<PostResponseDTO> findAllPostsByCurriculum(Long curriculumId) {
+//        List<Post> posts = postRepository.findByCurriculumId(curriculumId);
+//        return posts.stream().map(this::convertToResponseDTO).collect(Collectors.toList());
+//    }
 
     // UPDATE
     @Transactional
-    public PostResponseDTO updatePost(Long curriculumId, Long postId, PostRequestDTO requestDTO) {
-        Curriculum curriculum = curriculumRepository.findById(curriculumId)
-                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 커리큘럼 ID 입니다"));
-
+    public void updatePost(Long postId, PostRequestDTO requestDTO, Long curriculumId, Long memberId) {
         Post existingPost = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 id의 게시글을 찾을 수 없습니다."));
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, postId + " 가 없습니다."));
+
+        Curriculum curriculum = curriculumRepository.findById(curriculumId)
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, curriculumId + " 가 없습니다."));
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, memberId + " 가 없습니다."));
 
         Post updatedPost = existingPost.toBuilder()
                 .title(requestDTO.getTitle())
@@ -75,26 +82,27 @@ public class PostService {
                 .recruitNum(requestDTO.getRecruitNum())
                 .contactUrl(requestDTO.getContactUrl())
                 .status(requestDTO.getStatus())
-                .updatedAt(LocalDate.now())
                 .curriculum(curriculum)
+                .member(member)
+                .updatedAt(LocalDate.now())
                 .build();
 
         postRepository.save(updatedPost);
-        return convertToResponseDTO(updatedPost);
+        convertToResponseDTO(updatedPost);
     }
 
     // DELETE
     @Transactional
-    public void deletePost(Long curriculumId, Long postId) {
-        postRepository.findByCurriculumIdAndId(curriculumId, postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 id의 게시글을 찾을 수 없습니다."));
+    public void deletePost(Long postId) {
+        postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(ErrorCode.ID_NOT_FOUNT, postId + " 가 없습니다."));
         postRepository.deleteById(postId);
     }
 
-    private PostResponseDTO convertToResponseDTO(Post post) {
-        if (post == null) return null;
+    private void convertToResponseDTO(Post post) {
+        if (post == null) return;
 
-        return new PostResponseDTO(
+        new PostResponseDTO(
                 post.getId(),
                 post.getTitle(),
                 post.getContent(),
@@ -105,20 +113,13 @@ public class PostService {
                 post.getStatus(),
                 post.getCreatedAt(),
                 post.getUpdatedAt(),
-                post.getCurriculum().getName()
+                post.getCurriculum().getName(),
+                post.getMember().getName()
         );
     }
 
     private Post convertToEntity(PostRequestDTO requestDTO) {
-        if (requestDTO == null) return null;
-
-        Curriculum curriculum = null;
-        if (requestDTO.getCurriculumId() != null) {
-            curriculum = curriculumRepository.findById(requestDTO.getCurriculumId())
-                    .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 커리큘럼 ID 입니다: " + requestDTO.getCurriculumId()));
-        }
-
-        Post post = Post.builder()
+        return Post.builder()
                 .title(requestDTO.getTitle())
                 .content(requestDTO.getContent())
                 .classification(requestDTO.getClassification())
@@ -128,9 +129,6 @@ public class PostService {
                 .status(requestDTO.getStatus())
                 .createdAt(LocalDate.now())
                 .updatedAt(LocalDate.now())
-                .curriculum(curriculum)
                 .build();
-
-        return post;
     }
 }
