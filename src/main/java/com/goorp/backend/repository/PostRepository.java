c13ab2750f04378728da22a143e5721a0e658b10
hyunseong hwang <ree7243@gmail.com>
2023-09-14 14:59:16 +0900

    #21 - post repository생성
    post repository에 메서드 추가 및 controller 구현

--- src/main/java/com/goorp/backend/repository/PostRepository.java
+++ src/main/java/com/goorp/backend/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.goorp.backend.repository;
 
 import com.goorp.backend.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +17,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByCurriculumId(Long curriculumId);
 
+    @Transactional(readOnly = true)
+    @EntityGraph(attributePaths = {"curriculum", "member"})
+    Page<Post> findByMember_AccountId(String accountId, Pageable pageable);
 }
