31b9edc437c5a673333df6467f6e5f3c78c8bc33
wlstnam <wlstnam99@gmail.com>
2023-09-25 18:49:05 +0900

    #39 comment 도메인, dto, 예외처리 생성

---
+++ src/main/java/com/goorp/backend/domain/Comment.java
@@ -0,0 +1,42 @@
+package com.goorp.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @Column(name = "COMMENT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private int group;
+    @Column(nullable = false)
+    private int groupCnt;
+    @Column(nullable = false)
+    private int depth;
+    @Column(nullable = false)
+    private LocalDate createdAt;
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+}
