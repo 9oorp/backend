a6841d9755fab698bde3028ccc2924fa1a364868
hwang <ree7243@gmail.com>
2023-10-05 22:03:37 +0900

    CORS 오류를 처리

---
+++ src/main/java/com/goorp/backend/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.goorp.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://9oorp.store")  // 실제 필요한 출처를 지정
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
