bf7c9b3633704cb9096fa0a0a19d6dd0e728e067
hstla <ree7243@gmail.com>
2023-11-09 11:25:15 +0900

    #58-프리패스 리프레쉬 토큰 생성하는 테스트 코드 작성

--- src/test/java/com/goorp/backend/utils/FreePassToken.java
+++ src/test/java/com/goorp/backend/utils/FreePassToken.java
@@ -18,6 +18,15 @@ public class FreePassToken {
         String freePassToken = jwtUtil.createAccessToken("hello1", "홍길동(풀스택 2회차)", RoleType.USER,
             MaxExpireTimeMs);
         log.info("freePassToken = {}", freePassToken);
-        // eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50SWQiOiJoZWxsbzEiLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZKO2SgOyKpO2DnSAy7ZqM7LCoKSIsInJvbGUiOiJVU0VSIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTY5OTQyMzA0OCwiZXhwIjoyMDE0NzgzMDQ4fQ.tYF1GofuknHoS5ac-v9aRQVlVn9wdqXDICSlmMJsd_8
+        // eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50SWQiOiJoZWxsbzEiLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZKO2SgOyKpO2DnSAy7ZqM7LCoKSIsInJvbGUiOiJVU0VSIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTY5OTQ5NDMyNiwiZXhwIjoyMDE0ODU0MzI2fQ.UetqjvVyPET8mXn6zajkoDhO3R1pJcTKCUOYJBkq7R4
+    }
+
+    @Test
+    void createFreePassTokenByRefreshToken() {
+        long MaxExpireTimeMs = 1000L * 60 * 60 * 24 * 365 * 10;  // 10년
+
+        String refreshFreePassToken = jwtUtil.createRefreshToken("hello1", MaxExpireTimeMs);
+        log.info("refreshFreePassToken = {}", refreshFreePassToken);
+        // eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50SWQiOiJoZWxsbzEiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY5OTQ2MzQ0OCwiZXhwIjoyMDE0ODIzNDQ4fQ.ChMT1qZ6hGzUfQEYrXm0MWy8BUyEMc_UBkGvu1w6-S4
     }
 }
