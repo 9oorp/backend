7645311f09b4df0e9ac9975f3a78abb0d4aaf7ac
sbslc2000 <sbslc2000@gmail.com>
2023-11-15 18:25:58 +0900

    HotFix: ErrorCode 중복값 삭제

--- src/main/java/com/goorp/backend/exception/ErrorCode.java
+++ src/main/java/com/goorp/backend/exception/ErrorCode.java
@@ -16,11 +16,10 @@ public enum ErrorCode {
     ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, ""),
     NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, ""),
     NOT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, ""),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, ""),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, ""),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, ""),
-    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, ""),
-    NOT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "");
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "");
 
     private HttpStatus httpStatus;
     private String message;
