754fe7c7ad83418fc7ee6f20a75498b8acb41696
hstla <ree7243@gmail.com>
2023-11-20 19:45:42 +0900

    #78-안쓰는 오류코드삭제

--- src/main/java/com/goorp/backend/api/exception/ErrorCode.java
+++ src/main/java/com/goorp/backend/api/exception/ErrorCode.java
@@ -13,14 +13,10 @@ public enum ErrorCode {
     ID_NOT_FOUNT(HttpStatus.NOT_FOUND, ""),
     PASSWORD_NOT_SAME(HttpStatus.UNAUTHORIZED, ""),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
-    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, ""),
-    NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, ""),
-    NOT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, ""),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, ""),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, ""),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, ""),
+    NESTED_REPLY_EXCEPTION(HttpStatus.BAD_REQUEST, ""),
     NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "");
-
     private HttpStatus httpStatus;
     private String message;
 }
